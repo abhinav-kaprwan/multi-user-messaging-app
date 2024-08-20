53bf7cc4160dbfc77c5f64d5f45730dca752d668
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-08-20 18:35:03 +0530

    register route created

---
+++ app/api/register/route.ts
@@ -0,0 +1,26 @@
+import { db } from '@/db/db';
+import { users } from '@/db/schema';
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
+
+export async function post(request: Request) {
+    try {
+        const {email,name,password} = await request.json();
+        if(!email || !name || !password){
+            return new NextResponse('Missing info', {status:400});
+        }
+    
+        const hashedPassword = await bcrypt.hash(password, 12);
+    
+        const user = await db.insert(users).values({
+            email: email,
+            name: name, 
+            hashedPassword: hashedPassword,
+        });
+    
+        return NextResponse.json(user);
+    } catch (error:any) {
+        console.log(error, 'Registration_Error');
+        return new NextResponse('Internal Error', {status:500});
+    }
+}
