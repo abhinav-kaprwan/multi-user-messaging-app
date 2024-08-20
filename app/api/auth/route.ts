f5b283d9c99e9b4b895f9953bad64be948c7975b
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-08-20 18:06:07 +0530

    next auth route created

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,21 +1,19 @@
 import bcrypt from 'bcrypt';
-import NextAuth from 'next-auth';
+import NextAuth,{NextAuthConfig} from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from '@/db/db'; 
+import {users} from "@/db/schema";
 import { getProviders } from 'next-auth/react';
 import {eq} from 'drizzle-orm';
-import { use } from 'react';
-import { users } from '@/db/schema';
 
 
 
-export const authOptions = {
+export const authOptions:NextAuthConfig= {
     adapter: DrizzleAdapter(db),
-    secret: process.env.SECRET,
-    Providers:[
+    providers:[
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
@@ -33,27 +31,33 @@ export const authOptions = {
             async authorize(credentials){  
                 if(!credentials?.email || !credentials?.password){
                     throw new Error('Invalid Credentials')
-                } 
-                const hello = await db.query.users.findMany({
-                    where: eq(users.email, credentials.email)
-                  }) 
-               if(!users || !users?.hashedPassword) {
-                     throw new Error('Invalid Credentials')
-               }
+                }
+                const email = credentials.email as string;
+                const password = credentials.password as string;
+                const user = await db.select().from(users).where(eq(users.email, email));
 
-                const isCorrectPassword = await bcrypt.compare(
-                    credentials.password,
-                    hello.hashedPassword
-                )
-
-                if(!isCorrectPassword){
+                if(!user || !user[0].hashedPassword){
                     throw new Error('Invalid Credentials')
                 }
 
-                return user;
+                const isCorrectPassword = await bcrypt.compare(
+                    password,
+                    user[0].hashedPassword
+                );
+                
+                if(!isCorrectPassword){
+                    throw new Error('Invalid Credentials')
+                }
+                return user[0];
             }
         })
     ],
     debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
 
-}
+const handler = NextAuth(authOptions);
+export {handler as GET, handler as POST};
