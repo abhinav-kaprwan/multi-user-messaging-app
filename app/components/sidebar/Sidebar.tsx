51f8a2f19fa76df008a5a54a4ff4e18528e9ddb6
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-08-24 23:12:14 +0530

    useConversation hook created

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,7 +1,11 @@
+import DesktopSidebar from "./DesktopSidebar"
 async function Sidebar({children}:{children:React.ReactNode}){
     return(
         <div className="h-full">
-            {children}
+            <DesktopSidebar/>
+            <main className="lg:pl-20 h-full">
+                {children}
+            </main>
         </div>    
     )
 }
