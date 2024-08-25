ea52b4a62bb0ceb3d0d699e6b9cbd91190b1428a
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-08-25 13:56:08 +0530

    created bottom left avatar

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -2,10 +2,10 @@ import getCurrentUser from "@/app/actions/getCurrentUser"
 import DesktopSidebar from "./DesktopSidebar"
 import MobileFooter from "./MobileFooter"
 async function Sidebar({children}:{children:React.ReactNode}){
-    const currrentUser = await getCurrentUser();
+    const currentUser = await getCurrentUser();
     return(
         <div className="h-full">
-            <DesktopSidebar currentUser = {currentUser}/>
+            <DesktopSidebar currentUser = {currentUser!}/>
             <MobileFooter/>
             <main className="lg:pl-20 h-full">
                 {children}
