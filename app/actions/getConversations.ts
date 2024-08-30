c85e14a7acc1ff984acaf696fb95368922428dc0
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-08-30 22:14:15 +0530

    get conversation action created

--- app/actions/getConversations.ts
+++ app/actions/getConversations.ts
@@ -1,6 +1,7 @@
 import { db } from "@/db/db";
 import getCurrentUser from "./getCurrentUser";
-import { conversations } from "@/db/schema";
+import { conversations, userConversations,users,messages,userSeenMessages } from "@/db/schema";
+import { eq,desc} from "drizzle-orm";
 
 const getConversations = async () => {
     const currentUser = await getCurrentUser();
@@ -9,14 +10,44 @@ const getConversations = async () => {
     }
 
     try {
-        const allConversations = await db.query.conversations.findMany({
-            orderBy:{
-                lastMessage:"desc"
-            }
-        })
+      const senderAlias = users;
+      const result = await db
+      .select({
+        conversation: conversations,
+        user: users,
+        message: messages,
+        sender: senderAlias,
+        seenBy: userSeenMessages
+      })
+      .from(conversations)
+      .innerJoin(
+        userConversations,
+        eq(conversations.id, userConversations.conversationId)
+      )
+      .innerJoin(
+        users,
+        eq(userConversations.userId, users.id)
+      )
+      .leftJoin(
+        messages,
+        eq(conversations.id, messages.conversationId)
+      )
+      .leftJoin(
+        senderAlias,
+        eq(messages.senderId, senderAlias.id)
+      )
+      .leftJoin(
+        userSeenMessages,
+        eq(messages.id, userSeenMessages.messageId)
+      )
+      .where(
+        eq(userConversations.userId, currentUser.id)
+      )
+      .orderBy(desc(conversations.lastMessageAt));
         
-        
+      return result;
     } catch (error:any) {
         return [];
     }
 }
+export default getConversations;
