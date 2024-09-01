3011c99424a32102257521ce928de6d62478fd18
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-09-01 21:46:44 +0530

    defined types for processed data

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,4 +1,40 @@
 import { InferSelectModel } from 'drizzle-orm';
-import { users } from '@/db/schema';
+import { conversations, users } from '@/db/schema';
 
 export type User = InferSelectModel<typeof users>;
+
+
+export type Message = {
+    id: string;
+    senderId: string;
+    text: string;
+    createdAt: Date;
+    // Add other message properties as needed
+  };
+  
+  export type Conversation = {
+    id: string;
+    // Add other conversation properties as needed
+  };
+  
+  export type UserConversation = {
+    userId: string;
+    conversationId: string;
+    // Add other userConversation properties as needed
+  };
+  
+  export type SeenBy = {
+    userId: string;
+    messageId: string;
+    // Add other seenBy properties as needed
+  };
+    export type ProcessedMessage = Message & {
+    sender: User;
+  };
+  
+  export type ProcessedConversation = {
+    conversation: Conversation;
+    userConversation: UserConversation;
+    messages: ProcessedMessage[];
+    seenBy: SeenBy[];
+  };
